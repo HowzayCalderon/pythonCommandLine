0c216e4510372480f7a57473868db31f641bd9bf
hairyElephant <josecalderon353@gmail.com>
2023-02-04 18:37:27 -0500

    added extra functionality"

---
+++ app.py
@@ -0,0 +1,59 @@
+from peewee import *
+import psycopg2
+
+db = PostgresqlDatabase('flashcards', user='postgres', password='12345', host='localhost', port=5432)
+
+db.connect()
+
+class  BaseModel(Model):
+    class Meta: 
+        database = db
+
+class flashcards(BaseModel):
+    front = CharField()
+    back = CharField()
+
+
+# db.drop_tables(flashcards)
+# db.create_tables([flashcards])
+
+# React = flashcards(front='React', back='React is a Javascript Frontend framework')
+# React.save()
+# CSS = flashcards(front='CSS', back= 'CSS is a language for specifying how documents are presented to users')
+# CSS.save()
+
+
+# print(getFlashcard.front, getFlashcard.back)
+
+# This counter variable is to keep track of the card id
+counter = 1
+i = 5
+while i < 6:
+    action = input('What would you like to do? (create, study, quit)\n')
+
+    if action == 'study':
+        how_many = int(input('How many cards would you like to study?\n'))
+        for x in range(how_many):
+            getFlashcard = flashcards.get(flashcards.id == counter)
+
+            answer = input(f'{getFlashcard.front}\n')
+            
+            if answer == getFlashcard.back:
+                print('Correct')
+            else: 
+                print(f'Incorrect, the answer is: {getFlashcard.back}')
+
+            counter = counter + 1
+    elif action == 'create': 
+        f_card = input('Type the specific term the card will be about Ex. React or Data Structures\n')
+        b_card = input('Type the description of the term Ex. "MongoDb is a Database"\n')
+        new_card = flashcards(front= f_card, back= b_card)
+        new_card.save()
+    elif action == 'quit':
+        i = 7
+
+
+
+
+
+# ************ DONT FORGET TO ADD A WAY TO TRACK HOW MANY TIMES A PERSON GETS A SPECIFIC CARD WRONG *****************
